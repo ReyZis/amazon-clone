3a14429247e65a8a0dd02b817448ba783391fc8e
unknown <ramidz130@gmail.com>
2020-12-27 23:21:12 +0100

    doc: adding some necessary files

---
+++ src/Payment.js
@@ -0,0 +1,209 @@
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import CheckoutProduct from "./CheckoutProduct";
+import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
+import axios from "./axios";
+import { db } from "./firebase";
+
+function Payment() {
+    const [{ basket, user }, dispatch] = useStateValue();
+
+    const history = useHistory();
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
+    const [error, setError] = useState(null);
+    const [disabled, setDisabled] = useState(true);
+    const [clientSecret, setClientSecret] = useState(true);
+
+    // this is the 
+    /* useEffect(() => {
+        // generate the special stripe secret which allows us to charge a customer
+
+        const getClientSecret = async () => {
+            const response = await axios({
+                method: "POST",
+                // stripe the total in a currencies subunits
+                url: `/payment/create?total=${getBasketTotal(basket) * 100}`,
+            });
+            setClientSecret(response.data.clientSecret);
+        };
+
+        getClientSecret();
+    }, [basket]); */
+
+    // this is the true submit handler that should handle charging the client, but since this is just a testing project, i will use a fake handler below that will just simulate the stripe functionality, then add the orders to the firestore database 
+    /* const handleSubmit = async (event) => {
+        // do all the fancy stripe stuff
+        event.preventDefault();
+        setProcessing(true);
+
+        const payload = await stripe
+            .confirmCardPayment(clientSecret, {
+                payment_method: {
+                    card: elements.getElement(CardElement),
+                },
+            })
+            .then(({ paymentIntent }) => {
+                // paymentIntent = payment confirmation
+
+                db.collection("users")
+                    .doc(user?.uid)
+                    .collection("orders")
+                    .doc(paymentIntent.id)
+                    .set({
+                        basket: basket,
+                        amount: paymentIntent.amount,
+                        created: paymentIntent.created,
+                    });
+
+                setSucceeded(true);
+                setError(null);
+                setProcessing(false);
+
+                dispatch({
+                    type: "EMPTY_BASKET",
+                });
+
+                history.replace("/orders");
+            });
+    }; */
+
+    // this just a fake handler that should simulate the stripe fuctionality and add the orders to the database
+    const handleSubmit = async (event) => {
+        // do all the fancy stripe stuff
+        event.preventDefault();
+        setProcessing(true);
+
+        const payload = new Promise((res, rej) => {
+            res({
+                amount: getBasketTotal(basket),
+                created: new Date(),
+            });
+        }).then((paymentIntent) => {
+            // paymentIntent = payment confirmation
+
+            db.collection("users")
+                .doc(user?.uid)
+                .collection("orders")
+                .add({
+                    basket: basket,
+                    amount: paymentIntent.amount,
+                    created: paymentIntent.created,
+                })
+                .then(() => console.log("orders have been added succesfully"))
+                .catch((err) => console.log(err));
+
+            setSucceeded(true);
+            setError(null);
+            setProcessing(false);
+
+            dispatch({
+                type: "EMPTY_BASKET",
+            });
+
+            history.replace("/orders");
+        });
+    };
+
+    const handleChange = (event) => {
+        // listen for changes inside the card element
+        // and display any error as the costumer types their card details
+        setDisabled(event.empty);
+        setError(event.error ? event.error.message : "");
+    };
+
+    return (
+        <div className="payment">
+            <div className="payment__container">
+                <h1>
+                    Checkout (<Link to="checkout">{basket?.length} items</Link>)
+                </h1>
+
+                {/* payment section - delivery adress */}
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Delivery Adress</h3>
+                    </div>
+
+                    <div className="payment__adress">
+                        <p>{user?.email}</p>
+                        <p>123 React Lane</p>
+                        <p>Los Angeles, CA</p>
+                    </div>
+                </div>
+
+                {/* payment section - Review Items */}
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Review items and delivery</h3>
+                    </div>
+
+                    <div className="payment__items">
+                        {basket.map((item) => (
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                {/* payment section - Payment method */}
+                <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Payment Method</h3>
+                    </div>
+
+                    <div className="payment__details">
+                        {/* Stripe magic will goes here */}
+                        <form onSubmit={handleSubmit}>
+                            <CardElement onChange={handleChange} />
+
+                            <div className="payment__priceContainer">
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                        <h3>Order Total: {value}</h3>
+                                    )}
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"$"}
+                                />
+                                <button
+                                    disabled={
+                                        processing || disabled || succeeded
+                                    }
+                                >
+                                    <span>
+                                        {processing ? (
+                                            <p>Processing</p>
+                                        ) : (
+                                            "Buy Now"
+                                        )}
+                                    </span>
+                                </button>
+                            </div>
+
+                            {/* error */}
+                            {error && <div>{error}</div>}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Payment;
